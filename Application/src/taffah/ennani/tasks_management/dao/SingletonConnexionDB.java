fa73e55eb6736a97c04a6375406edcb6cdb98257
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-12-24 01:20:38 +0100

    Add files via upload

---
+++ Application/src/taffah/ennani/tasks_management/dao/SingletonConnexionDB.java
@@ -0,0 +1,18 @@
+package taffah.ennani.tasks_management.dao;
+
+import java.sql.*;
+
+public class SingletonConnexionDB {
+    private static Connection connection;
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public Connection getConnexion(){
+        return connection;
+    }
+}
