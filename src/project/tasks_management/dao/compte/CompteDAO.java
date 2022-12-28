a5478d14ddbe8512a3a9c79f244b64bf25b1b3ce
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-12-28 10:17:22 +0100

    Delete src/project/tasks_management directory

--- src/project/tasks_management/dao/compte/CompteDAO.java
+++
@@ -1,8 +0,0 @@
-package project.tasks_management.dao.compte;
-
-import project.tasks_management.dao.Dao;
-import project.tasks_management.entities.Compte;
-
-public interface CompteDAO extends Dao<Compte> {
-    Compte getCompteByEmailAndPassword(String email,String password);
-}
